54ddd52e871a00a2acf48fe7c1fed93f9325880b
Spanagiot <spanagiot95@gmail.com>
2020-04-20 19:52:22 +0300

    update public page

---
+++ docs/precache-manifest.29798a536a7039bc75cb62f02036cecd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7175abe07e1e760ae2f4ad16f0e0140a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "040c46b0e6b91f5e1a45",
+    "url": "./static/css/main.dbfa58a5.chunk.css"
+  },
+  {
+    "revision": "40094f88e426ab87200a",
+    "url": "./static/js/2.960ef59f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.960ef59f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "040c46b0e6b91f5e1a45",
+    "url": "./static/js/main.f3472fc0.chunk.js"
+  },
+  {
+    "revision": "12d35f59078e8451e14d",
+    "url": "./static/js/runtime-main.6be0f5f3.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8e487fc5f72c4309de1722b53dc2ee16.js"
+  "./precache-manifest.29798a536a7039bc75cb62f02036cecd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
