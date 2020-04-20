2a5cf52e028eeab020cd7043f8e864a2d003aabd
Spanagiot <spanagiot95@gmail.com>
2020-04-20 19:56:08 +0300

    change homepage to github pages

---
+++ docs/precache-manifest.dab9aa245a935c9d2eb7e91803fa3d1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45f6fd500073e670c64f4fb0bbd65d8f",
+    "url": "/giphy-explorer/index.html"
+  },
+  {
+    "revision": "ed252abc1a6dbd778599",
+    "url": "/giphy-explorer/static/css/main.dbfa58a5.chunk.css"
+  },
+  {
+    "revision": "40094f88e426ab87200a",
+    "url": "/giphy-explorer/static/js/2.960ef59f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/giphy-explorer/static/js/2.960ef59f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed252abc1a6dbd778599",
+    "url": "/giphy-explorer/static/js/main.55c46262.chunk.js"
+  },
+  {
+    "revision": "df86f0737c89bbddfcc5",
+    "url": "/giphy-explorer/static/js/runtime-main.47f2dab8.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.29798a536a7039bc75cb62f02036cecd.js"
+  "/giphy-explorer/precache-manifest.dab9aa245a935c9d2eb7e91803fa3d1d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/giphy-explorer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
