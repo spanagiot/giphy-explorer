35152c2579d40cddd240b5bc22ce39eebf895abd
Spanagiot <spanagiot95@gmail.com>
2020-04-20 19:37:38 +0300

    reset search term state on clear

--- src/App.js
+++ src/App.js
@@ -88,6 +88,7 @@ class App extends React.Component {
           trending: false,
           search: false,
           GIFtotalCount: 0,
+          searchTerm: '',
         },
         this.storeStateInSessionStorage
       );
